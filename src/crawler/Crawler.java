21bad16a1cbe6057ca9f9253f62aa6d143fe7524
Will Hawkins <whh8b@virginia.edu>
2014-09-11 11:45:47 -0400

    Support creating output directories.

--- src/crawler/Crawler.java
+++ src/crawler/Crawler.java
@@ -4,6 +4,9 @@ import java.lang.System;
 import indexes.Index4WebMD;
 import java.util.ArrayList;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Crawl a set of WebMD exchange topics.
  *
@@ -14,12 +17,38 @@ public class Crawler {
 	public static void main(String args[]) {
 		ArrayList<String> indexes = new ArrayList<String>();
 
+		try {
+			Files.createDirectory(Paths.get("./data/"));
+		} catch (Exception e) {
+		}
+
+		try {
+			Files.createDirectory(Paths.get("./data/json/"));
+			Files.createDirectory(Paths.get("./data/HTML/"));
+		} catch (Exception e) {
+		}
+
+		try {
+			Files.createDirectory(Paths.get("./data/json/WebMD/"));
+			Files.createDirectory(Paths.get("./data/HTML/WebMD/"));
+		} catch (Exception e) {
+		}
+
 		indexes.add("anxiety-and-panic-disorders");
 		indexes.add("parenting");
 		indexes.add("heart-disease");
 
 		for (String index : indexes) {
 			int i = 1;
+
+			/*
+			 * Make sure output paths exist.
+			 */
+			try {
+				Files.createDirectory(Paths.get("./data/HTML/WebMD/" + index));
+				Files.createDirectory(Paths.get("./data/json/WebMD/" + index));
+			} catch (Exception e) {
+			}
 			/*
 			 * It takes about 40 pages to get 1000
 			 * threads.
